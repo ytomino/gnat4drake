4b0ea897d6ae705779e82e24783f85f9d7ebcc26
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-06-04 02:49:05 +0900

    Add GNAT.Strings

---
+++ g-string.ads
@@ -0,0 +1,14 @@
+with Ada.Unchecked_Deallocation;
+package GNAT.Strings is
+   pragma Preelaborate;
+
+   type String_Access is access all String;
+
+   procedure Free is
+      new Ada.Unchecked_Deallocation (String, String_Access);
+
+   type String_List is array (Positive range <>) of String_Access;
+
+   type String_List_Access is access all String_List;
+
+end GNAT.Strings;
