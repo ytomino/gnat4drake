6f6735018f48238ce0ff0f84bd9db2adfbd88822
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-06-09 17:33:53 +0900

    Add some entities of GNAT.Directory_Operations

--- g-dirope.adb
+++ g-dirope.adb
@@ -1,5 +1,21 @@
 package body GNAT.Directory_Operations is
 
+   procedure Make_Dir (Dir_Name : Dir_Name_Str) is
+   begin
+      Ada.Directories.Create_Directory (Dir_Name);
+   end Make_Dir;
+
+   procedure Remove_Dir (
+      Dir_Name : Dir_Name_Str;
+      Recursive : Boolean := False) is
+   begin
+      if Recursive then
+         Ada.Directories.Delete_Tree (Dir_Name);
+      else
+         Ada.Directories.Delete_Directory (Dir_Name);
+      end if;
+   end Remove_Dir;
+
    function Base_Name (Path : Path_Name; Suffix : String := "")
       return String
    is
@@ -17,5 +33,43 @@ package body GNAT.Directory_Operations is
       return Ada.Hierarchical_File_Names.Simple_Name (
          Path (Path'First .. Last));
    end Base_Name;
+
+   function Format_Pathname (
+      Path : Path_Name;
+      Style : Path_Style := System_Default)
+      return Path_Name
+   is
+      pragma Unreferenced (Style);
+   begin
+      return Path;
+   end Format_Pathname;
+
+   procedure Open (Dir : in out Dir_Type; Dir_Name : Dir_Name_Str) is
+   begin
+      Ada.Directories.Start_Search (Dir, Dir_Name);
+   end Open;
+
+   procedure Read (
+      Dir : in out Dir_Type;
+      Str : out String;
+      Last : out Natural) is
+   begin
+      if Ada.Directories.More_Entries (Dir) then
+         declare
+            Directory_Entry : Ada.Directories.Directory_Entry_Type;
+         begin
+            Ada.Directories.Get_Next_Entry (Dir, Directory_Entry);
+            declare
+               Simple_Name : constant String :=
+                  Ada.Directories.Simple_Name (Directory_Entry);
+            begin
+               Last := Str'First + (Simple_Name'Length - 1);
+               Str (Str'First .. Last) := Simple_Name;
+            end;
+         end;
+      else
+         Last := 0; -- end of directory
+      end if;
+   end Read;
 
 end GNAT.Directory_Operations;
--- g-dirope.ads
+++ g-dirope.ads
@@ -1,7 +1,26 @@
+with Ada.Directories;
 with Ada.Hierarchical_File_Names;
 package GNAT.Directory_Operations is
 
    subtype Dir_Name_Str is String;
+
+   subtype Dir_Type is Ada.Directories.Search_Type;
+
+   Directory_Error : exception;
+
+   --  Basic Directory operations
+
+   procedure Change_Dir (Dir_Name : Dir_Name_Str)
+      renames Ada.Directories.Set_Directory;
+
+   procedure Make_Dir (Dir_Name : Dir_Name_Str);
+
+   procedure Remove_Dir (
+      Dir_Name : Dir_Name_Str;
+      Recursive : Boolean := False);
+
+   function Get_Current_Dir return Dir_Name_Str
+      renames Ada.Directories.Current_Directory;
 
    --  Pathname Operations
 
@@ -12,5 +31,27 @@ package GNAT.Directory_Operations is
 
    function Base_Name (Path : Path_Name; Suffix : String := "")
       return String;
+
+   function File_Extension (Path : Path_Name) return String
+      renames Ada.Hierarchical_File_Names.Extension;
+
+   type Path_Style is (System_Default);
+
+   function Format_Pathname (
+      Path : Path_Name;
+      Style : Path_Style := System_Default)
+      return Path_Name;
+
+   --  Iterators
+
+   procedure Open (Dir : in out Dir_Type; Dir_Name : Dir_Name_Str);
+
+   procedure Close (Dir : in out Dir_Type)
+      renames Ada.Directories.End_Search;
+
+   procedure Read (
+      Dir : in out Dir_Type;
+      Str : out String;
+      Last : out Natural);
 
 end GNAT.Directory_Operations;
