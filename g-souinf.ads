5b03f7dc523396b78d32b87d28a2622aa0b210c2
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-06-09 09:41:33 +0900

    Add GNAT.Source_Info.Source_Location and Enclosing_Entity

--- g-souinf.ads
+++ g-souinf.ads
@@ -1,8 +1,14 @@
 with Ada;
 package GNAT.Source_Info is
    pragma Pure;
+
    function File return String
       renames Ada.Debug.File;
    function Line return Integer
       renames Ada.Debug.Line;
+   function Source_Location return String
+      renames Ada.Debug.Source_Location;
+   function Enclosing_Entity return String
+      renames Ada.Debug.Enclosing_Entity;
+
 end GNAT.Source_Info;
