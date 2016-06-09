74119b2a74ffdfb126e60e139901dcbdc4083825
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-06-09 14:26:12 +0900

    Add System.CRTL

---
+++ s-crtl.ads
@@ -0,0 +1,18 @@
+with C.stdlib;
+package System.CRTL is
+   pragma Preelaborate;
+
+   subtype size_t is C.size_t;
+
+   --  Other C runtime functions
+
+   procedure free (Ptr : System.Address)
+      renames C.stdlib.free;
+
+   function malloc (Size : size_t) return System.Address
+      renames C.stdlib.malloc;
+
+   function realloc (Ptr : System.Address; Size : size_t) return System.Address
+      renames C.stdlib.realloc;
+
+end System.CRTL;
