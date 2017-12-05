ffd7936f3aefc6aeea959b6639c0334426a0d15a
ytomino <demoonlit@panathenaia.halfmoon.jp>
2017-12-05 18:50:30 +0900

    Add some entities of Interfaces.C_Streams

--- i-cstrea.ads
+++ i-cstrea.ads
@@ -3,6 +3,8 @@ with C.stdio;
 package Interfaces.C_Streams is
    pragma Preelaborate;
 
+   subtype int is C.signed_int;
+
    subtype FILEs is C.stdio.FILE_ptr;
 
    function stdin return FILEs;
@@ -10,5 +12,8 @@ package Interfaces.C_Streams is
    function stderr return FILEs;
 
    --  Note: FILE * is not used in drake.
+
+   function fflush (stream : access C.stdio.FILE) return int
+      renames C.stdio.fflush;
 
 end Interfaces.C_Streams;
